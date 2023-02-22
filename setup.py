2535bfd8166ff3c5125daa5ac78f53a24b7a88fe
Marcus Bloice <marcus.bloice@medunigraz.at>
2023-02-22 17:45:40 +0100

    Added Python 3.11 to supported versions in setup.py

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@ Programming Language :: Python :: 3.7
 Programming Language :: Python :: 3.8
 Programming Language :: Python :: 3.9
 Programming Language :: Python :: 3.10
+Programming Language :: Python :: 3.11
 Programming Language :: Python :: 3 :: Only
 Topic :: Software Development
 Topic :: Scientific/Engineering
